1176d6bb50e170065419f0043e140a8660140a2c
bfallc <bfallc@artic.edu>
2020-01-28 17:00:59 -0600

    updates sketch again

--- docs/code/in_class/day01/01_RozinMirror/scripts/main.js
+++ docs/code/in_class/day01/01_RozinMirror/scripts/main.js
@@ -1,44 +1,55 @@
-//Day01
+//Day01, #4
 let myCapture;
 
 function setup() {
-  //createCanvas(320, 240);
-  let canvas = createCanvas(640, 480);
+  //createCanvas(640, 480);
+  //background(50);
+  let canvas=createCanvas(640, 480);
   canvas.parent("sketch");
   myCapture = createCapture(VIDEO);
   myCapture.hide();
   fill(0, 255, 0);
-  noStroke();
+  rectMode(CENTER);
+  //noStroke();
 }
 
 function draw() {
-  background(255);
+  background(50);
   //fill(random(255),random(255),random(255));
   //load pixel data into myCapture object.
   myCapture.loadPixels();
 
   //const stepSize = round(constrain(mouseX / 8, 25, 32));
   const stepSizeX = 16;
-  const stepSizeY =48;
+  const stepSizeY =16;
   for (let y = 0; y < height; y += stepSizeY) {
     for (let x = 0; x < width; x += stepSizeX) {
       //fill(random(255),random(255),random(255));
       const i = y * width + x;
       const darknessR = (255 - myCapture.pixels[i * 4]) / 255;
-      const radiusRX = stepSizeX * darknessR;
-      const radiusRY = stepSizeY * darknessR;
-
+      let radiusRX = stepSizeX * darknessR;
+      radiusRX = constrain(radiusRX, 8, 16);
+      let radiusRY = stepSizeY * darknessR;
+      radiusRY = constrain(radiusRY, 8, 16);
+      
       push();
-      translate(width,0);
+      translate(width-10,10);
       scale(-1,1);
       
       //if(x==0 && y==0){
       //print(darkness);
       //}
-      fill((darknessR*255),0,0);
-      ellipse(x, y, radiusRX, radiusRY);
+      push();
+        translate(x,y);
+        rotate((darknessR));
+        //fill(255-(darknessR*255)/20);
+      fill(255-(darknessR*255));
+      //fill(255);  
+      //rect(0, 0, radiusRX+8, radiusRX+8);
+      rect(0, 0, 20, 20);
+      pop();
+
       pop();
     }
   }
-
 }
