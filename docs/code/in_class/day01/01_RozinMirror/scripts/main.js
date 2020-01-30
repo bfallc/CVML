ea64b2339e1c7ee6b0f85833e2820f03bb8cc5e4
bfallc <60400018+bfallc@users.noreply.github.com>
2020-01-30 10:03:09 -0600

    Update main.js

--- docs/code/in_class/day01/01_RozinMirror/scripts/main.js
+++ docs/code/in_class/day01/01_RozinMirror/scripts/main.js
@@ -1,14 +1,14 @@
-//Day01, #4
+//Day01, #5
 let myCapture;
 
 function setup() {
-  //createCanvas(640, 480);
+  createCanvas(640, 480);
   //background(50);
-  let canvas=createCanvas(640, 480);
-  canvas.parent("sketch");
+  //let canvas=createCanvas(640, 480);
+  //canvas.parent("sketch");
   myCapture = createCapture(VIDEO);
   myCapture.hide();
-  fill(0, 255, 0);
+  //fill(0, 255, 0);
   rectMode(CENTER);
   //noStroke();
 }
@@ -21,7 +21,7 @@ function draw() {
 
   //const stepSize = round(constrain(mouseX / 8, 25, 32));
   const stepSizeX = 16;
-  const stepSizeY =16;
+  const stepSizeY = 16;
   for (let y = 0; y < height; y += stepSizeY) {
     for (let x = 0; x < width; x += stepSizeX) {
       //fill(random(255),random(255),random(255));
@@ -31,25 +31,35 @@ function draw() {
       radiusRX = constrain(radiusRX, 8, 16);
       let radiusRY = stepSizeY * darknessR;
       radiusRY = constrain(radiusRY, 8, 16);
-      
+
       push();
-      translate(width-10,10);
-      scale(-1,1);
-      
+      translate(width - 10, 10);
+      scale(-1, 1);
+
       //if(x==0 && y==0){
       //print(darkness);
       //}
       push();
-        translate(x,y);
-        rotate((darknessR));
-        //fill(255-(darknessR*255)/20);
-      fill(255-(darknessR*255));
+      translate(x, y);
+      rotate((darknessR));
+      //fill(255-(darknessR*255)/20);
+      let grey = 255-(darknessR * 255);
+      fill(grey);
       //fill(255);  
       //rect(0, 0, radiusRX+8, radiusRX+8);
+
+
       rect(0, 0, 20, 20);
+      //translate(width - 10, 10);
+      // scale(-1, 1);
+      // fill(grey);
+      // textSize(10);
+      // textStyle(BOLD);
+      // text(grey, -2, 2);
       pop();
 
       pop();
     }
   }
+
 }
